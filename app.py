55bbcd68c1b231af2e17045d7bad35c80be5f13d
jj123230 <42804892+jj123230@users.noreply.github.com>
2022-06-29 16:17:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ import time
 import pandas as pd
 from datetime import datetime
 import urllib.request
-import io
 
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
